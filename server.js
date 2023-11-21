e731fa30f0ca2a4397b26ec512e17f893436b1c1
Himanshu Chouhan <himanshu12320000@gmail.com>
2023-11-21 14:21:10 +0530

    fix minor issue

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 const ChatData = require("./Models/chatting.js");
+const { log } = require("console");
 
 connectDB();
 
@@ -24,13 +25,19 @@ const users = {}; // to store user sockets
 io.on("connection", (socket) => {
   console.log(`A user connected - ${socket.id}`);
 
-  socket.on("signin", (userEmailId) => {
-    users[userEmailId] = socket.id;
+  let userEmailId = "him1@g.com";
+
+  socket.on("signin", (data) => {
+    const { emailId } = data; 
+    log("event sign in callled");
+    users[emailId] = socket.id;
+    userEmailId = emailId;
+    getNewMessage(userEmailId);
   });
 
-  getNewMessage(userEmailId);
 
-  socket.on("message", (data) => {
+  // server rereciving message from frontend
+  socket.on("chat", (data) => {
     const { receiverEmailId, message } = data;
 
     saveMessage(userEmailId, receiverEmailId, message, false, "post");
@@ -43,8 +50,11 @@ io.on("connection", (socket) => {
         message,
       });
     } else {
-      saveNewMessage(receiverEmailId, senderEmailId, message, true);
-      console.log(`User ${receiverId} is offline. Save the message for later.`);
+
+      // saving in reciver database as new message 
+
+      saveMessage(receiverEmailId, userEmailId, message, true, "get" );
+      console.log(`User ${receiverEmailId} is offline. Save the message for later.`);
     }
   });
 
@@ -63,12 +73,17 @@ io.on("connection", (socket) => {
 
 
 async function getNewMessage(userEmailId) {
+  console.log("get new Mesage called");
   let userChatData;
 
   userChatData = await ChatData.findOne({
     me: userEmailId,
     isNewMessage: true,
   });
+
+  if(!userChatData){
+    return;
+  }
 
   const mySocketId = users[userEmailId];
 
@@ -98,6 +113,7 @@ async function getNewMessage(userEmailId) {
 }
 
 async function saveMessage(myEmailId, friendEmailId, message, isNew, type) {
+  console.log("Save message called");
   let myChat;
   //let friendChat;
 
@@ -105,7 +121,9 @@ async function saveMessage(myEmailId, friendEmailId, message, isNew, type) {
   myChat = await ChatData.findOne({ me: myEmailId, isNewMessage: isNew });
   //friendChat = await ChatData.findOne({me : friendEmailId , isNewMessage : isNew});
 
-  // if new create user
+  console.log(myChat);
+
+  // if i am a new user
   if (!myChat) {
     myChat = await ChatData.create({
       me: myEmailId,
@@ -120,19 +138,42 @@ async function saveMessage(myEmailId, friendEmailId, message, isNew, type) {
     })
   }
 */
+// if chat with friend is first time create it
+
+// uncomment below
+/*
+log("check chat" ,[friendEmailId] in myChat )
   if (!([friendEmailId] in myChat)) {
-    myChat[friendEmailId] = [];
+    log("inside if")
+
+
+    const resp = await ChatData.updateOne({"me" : myEmailId, isNewMessage : isNew},{$set:{[friendEmailId] : ["demo","delete1"]} })
+
+    //myChat.sample = "sample";
+
+    
+   
+    log(`my chat empty arr `,myChat);
+   
+    //await myChat.save;
+
   }
+*/
+
+  
   /*
   if( !([myEmailId] in friendChat) ){
     friendChat[myEmailId] = [];
   }
 */
+/*
+// Adding chat message
   myChat[friendEmailId].push({
     message: message,
-    time: Data.now(),
+    time: Date.now(),
     type: type,
   });
+  log(`my chat ${myChat}`);
   /*
   var revType = (type === "get") ? "post" : "get";
 
@@ -142,9 +183,16 @@ async function saveMessage(myEmailId, friendEmailId, message, isNew, type) {
     type : revType
   });
 */
+
+
+
+
+// uncomment below
+/*
   await myChat.save();
   // await friendChat.save();
   return;
+  */
 }
 
 server.listen(4001, () => {
