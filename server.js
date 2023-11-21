062854dfbdc32994ae930f35ce55223db726619c
Himanshu Chouhan <himanshu12320000@gmail.com>
2023-11-21 21:57:14 +0530

    chat update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const users = {}; // to store user sockets
 io.on("connection", (socket) => {
   console.log(`A user connected - ${socket.id}`);
 
-  let userEmailId = "him1@g.com";
+  let userEmailId;
 
   socket.on("signin", (data) => {
     const { emailId } = data; 
@@ -35,41 +35,42 @@ io.on("connection", (socket) => {
     getNewMessage(userEmailId);
   });
 
+  // io.emit('hi',{message : "hi"})
 
   // server rereciving message from frontend
-  socket.on("chat", (data) => {
+  socket.on("chat", async (data) => {
     const { receiverEmailId, message } = data;
 
-    saveMessage(userEmailId, receiverEmailId, message, false, "post");
+    await saveMessage(userEmailId, receiverEmailId, message, false, "post");
 
-    // is receiver online
     const receiverSocketId = users[receiverEmailId];
+
     if (receiverSocketId) {
-      io.to(receiverSocketId).emit("messageEmit", {
+
+      console.log("inside if")
+
+      io.to(receiverSocketId).emit("sendMsg", {
         sender: userEmailId,
         message,
       });
+
+      await saveMessage(receiverEmailId, userEmailId, message, false, "get");
+
     } else {
 
       // saving in reciver database as new message 
 
-      saveMessage(receiverEmailId, userEmailId, message, true, "get" );
+      await saveMessage(receiverEmailId, userEmailId, message, true, "get" );
       console.log(`User ${receiverEmailId} is offline. Save the message for later.`);
     }
-  });
-
-  socket.on("messageEmit", (data) => {
-    const { sender, message } = data;
-
-    saveMessage(userEmailId, sender, message, false, "get");
   });
 
   // Handle disconnection
   socket.on("disconnect", () => {
     console.log("User disconnected");
+    delete users[userEmailId];
   });
 });
-
 
 
 async function getNewMessage(userEmailId) {
@@ -81,47 +82,60 @@ async function getNewMessage(userEmailId) {
     isNewMessage: true,
   });
 
+  // log(userChatData, userEmailId);
+
   if(!userChatData){
     return;
   }
 
+  log("here");
+
   const mySocketId = users[userEmailId];
 
   var senderEmails = Object.keys(userChatData);
+  for (var value of senderEmails){
+    
+    if (!value.includes('@')) {continue}
+    
+    log("in loop")
+    log(value);
 
-  for (var value of senderEmails) {
-    if (
-      value === "isNewMessage" ||
-      value === "lastSeen" ||
-      value === "receiver"
-    ) {
-      continue;
-    }
-
-    const senderEmail = value;
-
-    userChatData[value].map((ele) => {
-      io.to(mySocketId).emit("messageEmit", {
+    for (var ele of userChatData[value]){
+      io.to(mySocketId).emit("sendMsg", {
         sender: value,
         message: ele.message,
       });
-      return {};
-    });
 
-    userChatData[value] = [];
+      await saveMessage(userEmailId,value,ele.message,false,"get");
+    }
+/*
+    userChatData[value].map(async (ele) => {
+      io.to(mySocketId).emit("sendMsg", {
+        sender: value,
+        message: ele.message,
+      });
+
+      await saveMessage(userEmailId,value,ele.message,false,"get");
+    })
+  ;
+  */
   }
+
+  await ChatData.deleteOne({me : userEmailId});
+
 }
 
 async function saveMessage(myEmailId, friendEmailId, message, isNew, type) {
   console.log("Save message called");
   let myChat;
-  //let friendChat;
+  
+  const friendEmailIdFilter = friendEmailId.replaceAll('.','@dot@');
+  const myEmailIdFilter = myEmailId.replaceAll('.','@dot@');
 
   // find user chat
   myChat = await ChatData.findOne({ me: myEmailId, isNewMessage: isNew });
-  //friendChat = await ChatData.findOne({me : friendEmailId , isNewMessage : isNew});
-
-  console.log(myChat);
+  
+  // console.log(myChat);
 
   // if i am a new user
   if (!myChat) {
@@ -130,69 +144,18 @@ async function saveMessage(myEmailId, friendEmailId, message, isNew, type) {
       isNewMessage: isNew,
     });
   }
-  /*
-  if(!friendChat){
-    friendChat = await ChatData.create({
-      me : friendEmailId,
-      isNewMessage : isNew,
-    })
-  }
-*/
-// if chat with friend is first time create it
-
-// uncomment below
-/*
-log("check chat" ,[friendEmailId] in myChat )
-  if (!([friendEmailId] in myChat)) {
-    log("inside if")
 
 
-    const resp = await ChatData.updateOne({"me" : myEmailId, isNewMessage : isNew},{$set:{[friendEmailId] : ["demo","delete1"]} })
-
-    //myChat.sample = "sample";
-
-    
-   
-    log(`my chat empty arr `,myChat);
-   
-    //await myChat.save;
-
-  }
-*/
-
-  
-  /*
-  if( !([myEmailId] in friendChat) ){
-    friendChat[myEmailId] = [];
-  }
-*/
-/*
-// Adding chat message
-  myChat[friendEmailId].push({
-    message: message,
-    time: Date.now(),
-    type: type,
-  });
-  log(`my chat ${myChat}`);
-  /*
-  var revType = (type === "get") ? "post" : "get";
-
-  friendChat[myEmailId].push({
-    message : message,
-    time : Data.now(),
-    type : revType
-  });
-*/
-
-
-
-
-// uncomment below
-/*
-  await myChat.save();
-  // await friendChat.save();
-  return;
-  */
+  // pushing chat
+  const resp = await ChatData.updateOne({"me" : myEmailId, isNewMessage : isNew},{$push:{
+    [friendEmailIdFilter] : {
+        message: message,
+        time: Date.now(),
+        type: type,
+      }
+      }
+      }, {upsert : true})
+      log("save msg over");
 }
 
 server.listen(4001, () => {
